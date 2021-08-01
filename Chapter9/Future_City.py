4cc53be9379a9a93f6133ff1d27fe33b0ea4fb88
yoonj98 <69336270+yoonj98@users.noreply.github.com>
2021-08-01 23:13:56 +0900

    Future City

---
+++ Chapter9/Future_City.py
@@ -0,0 +1,31 @@
+# 미래도시 (259p)
+
+INF = int(1e9) 
+
+n, m = map(int, input().split())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+x, k = map(int, input().split())
+
+# 점화식 - 플로이드 워셜 알고리즘
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+distance = graph[1][k] + graph[k][x]
+
+if distance >= 1e9:
+    print("-1")
+else:
+    print(distance)
