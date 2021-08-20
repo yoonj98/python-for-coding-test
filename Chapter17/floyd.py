6ee4407a9b8d7b3edc41a52760f50c2d34e23b82
YoonJeong Lee <69336270+yoonj98@users.noreply.github.com>
2021-08-20 21:55:54 +0900

    floyd

---
+++ Chapter17/floyd.py
@@ -0,0 +1,32 @@
+# 플로이드
+
+INF = int(1e9) # 무한을 의미
+
+n = int(input())
+m = int(input())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print(0, end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
